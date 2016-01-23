ffbcb69f4aa3fffc49ad6e98bdeca415c0b9a0bf
Chelsea Johnson <cheljoh@gmail.com>
2016-01-23 16:58:35 -0700

    finished main section plus one iteration

--- webguesser.rb
+++ webguesser.rb
@@ -6,29 +6,33 @@ require 'sinatra/reloader'
 SECRET_NUMBER = rand(100)
   get '/' do
     guess = params["guess"].to_i
-    message = check_guess(guess)
+    message, color = check_guess(guess)
     erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color}
   end
 
+
   def check_guess(guess)
-    if guess > number
-      if guess - number > 5
-        color = "red"
-        message = "Way too high!"
-      else
-        color = "pink"
-        message = "Too high!"
-      end
-    elsif guess < number
-      if number - guess > 5
-        color = "blue"
-        message = "Way too low!"
-      else
-        color = "#00afa9"
-        message = "Too low!"
-      end
+    if guess > SECRET_NUMBER
+      high_guess(guess)
+    elsif guess < SECRET_NUMBER
+      low_guess(guess)
     else
-      message = "You won, yay!\n The secret number is #{number}"
-      color = "green"
+      message = "You won, yay!\n The secret number is #{SECRET_NUMBER}", color = "green"
+    end
+  end
+
+  def high_guess(guess)
+    if guess - SECRET_NUMBER > 5
+      message = "Way too high!", color = "red"
+    else
+      message = "Too high!", color = "pink"
+    end
+  end
+
+  def low_guess(guess)
+    if SECRET_NUMBER - guess > 5
+      message = "Way too low!", color = "blue"
+    else
+      message = "Too low!", color = "#00afa9"
     end
   end
